a453f1d59bdcbbd315673e5e94d1768884dc9668
Ranjit K <ranjitklive@gmail.comgit config --global user.email ranjitklive@gmail.comgit config --global user.email ranjitklive@gmail.com>
2022-04-18 20:59:14 +0530

    init

---
+++ Code/Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// you can also use other imports, for example:
+using System.Collections.Generic;
+
+// you can write to stdout for debugging purposes, e.g.
+// Console.WriteLine("this is a debug message");
+
+class Solution
+{
+    public int solution(int[] A)
+    {
+        // write your code in C# 6.0 with .NET 4.5 (Mono)
+        int result = 0;
+        int arrLength = A.Length;
+
+        for (int i = 0; i < arrLength; i++)
+        {
+            for (int count = 0; i + 2 < arrLength && A[i + 1] - A[i] == A[i + 2] - A[i + 1]; i++)
+            {
+                count++;
+                result += count;
+            }
+        }
+
+        return result > 1000000000 ? -1 : result;
+    }
+}
---
+++ Code/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Solution s = new Solution();
+
+            int sol = s.solution(new int[] { 1, 3, 4, 1 });
+
+            Console.WriteLine(sol);
+        }
+    }
+
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int returnVal = -1;
+
+            if (A != null && A.Length <= 100000)
+            {
+                // write your code in C# 6.0 with .NET 4.5 (Mono)
+                List<int> numbers = new List<int>();
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    numbers.Add(A[i]);
+                }
+
+                A = numbers.Distinct().OrderBy(t=>t).ToArray();
+
+                for (int i = 0; i < A.Length; i++)
+                {
+                    if (i + 1 == A[i])
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        returnVal = i + 1;
+                        break;
+                    }
+                }
+            }
+
+            return returnVal;
+        }
+    }
+}
